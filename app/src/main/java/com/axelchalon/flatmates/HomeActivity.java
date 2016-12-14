1535d14be2db4adca043dedffbf8d4edb34013e4
Axel Chalon <axel.chalon@hetic.net>
2016-12-14 12:13:49 +0100

    getFlatmates() @ home json

--- app/src/main/java/com/axelchalon/flatmates/HomeActivity.java
+++ app/src/main/java/com/axelchalon/flatmates/HomeActivity.java
@@ -41,6 +41,7 @@ public class HomeActivity extends AppCompatActivity {
 
         getHighscores();
         getTasks();
+        getFlatmates();
     }
 
     protected void getHighscores() {
@@ -113,6 +114,54 @@ public class HomeActivity extends AppCompatActivity {
                             for(int i=0;i<scores.length();i++){
                                 JSONObject jsob = scores.optJSONObject(i);
                                 System.out.println(jsob.getString("id") + ". " + jsob.getString("task") + " : " + Integer.toString(jsob.getInt("weight")));
+                                // @todo hydrate view
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("WCC");
+                        error.printStackTrace();
+
+                        Context context = getApplicationContext();
+                        CharSequence text = "Impossible de se connecter au réseau, camarade !";
+                        int duration = Toast.LENGTH_LONG;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+
+                        VolleyLog.e("Error: ", error.getMessage());
+                    }
+                });
+
+        Volley.newRequestQueue(getApplicationContext()).add(myUserRequest);
+    }
+
+    protected void getFlatmates() {
+
+        SharedPreferences prefs = getSharedPreferences(USER_PREFS, MODE_PRIVATE);
+        String usernum = prefs.getString("usernum", null);
+
+        HashMap<String,String> params = new HashMap<String, String>();
+
+        final Context ctx = this;
+        JsonObjectRequest myUserRequest = new JsonObjectRequest
+                (Request.Method.GET, "http://swarm.ovh:4/index.php?action=get_flatmates&user_num=" + usernum, new JSONObject(params), new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        System.out.println("FLATMATES");
+                        try {
+                            JSONArray scores = response.getJSONArray("flatmates");
+
+                            for(int i=0;i<scores.length();i++){
+                                JSONObject jsob = scores.optJSONObject(i);
+                                System.out.println(jsob.getString("num") + ". " + jsob.getString("name"));
                                 // @todo hydrate view
                             }
                         } catch (JSONException e) {
